fd3be079f363f5e1b8b241ca4f5fd6060ef57b04
Shubh Umre <umreshubh6@gmail.com>
2026-01-04 17:27:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,64 @@
+import joblib
+import streamlit as st
+import nltk 
+from nltk.corpus import stopwords
+import string
+from nltk.stem.porter import PorterStemmer
+import pandas as pd
+
+# This object is used to do stemming of the words , basically to bring the words to their root words .
+ps = PorterStemmer()
+
+
+def text_pre_process(text) :
+    # This will make the text to lower case .
+    text = text.lower()
+    # This will break the text to tokens . 
+    text = nltk.word_tokenize(text)
+    
+    # This helps to remove the special characters like (( , @ # $ % ^ , etc...) , newlines and spaces also .
+    y = []
+    for i in text :
+        if i.isalnum() :
+            y.append(i)
+
+    # This removes Stopwords and Punctuations from the text .
+    text = y[:] # here we are copying the y and then clearing it in next step , hence the complete data of y is stored in text .
+    y.clear()
+    for i in text :
+        if i not in stopwords.words('english') and i not in string.punctuation :
+            y.append(i)
+
+    # This loop does stemming of the words in the text (STEMMINg means converting words to their root words .)
+    text = y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
+
+    # This join methods make a complete sentence from the list of words like : ["Hello" , "World"] --> Hello World. 
+    return " ".join(y)
+
+model = joblib.load("spam_model.joblib")
+# vectorizer = joblib.load("vectorizer.joblib")
+
+
+st.title("Spam Emial Classifier")
+user_text = st.text_area("Enter your email/message text:")
+
+if st.button("Check"):
+    if user_text.strip() == "":
+        st.warning("Please enter a message.")
+    else:
+        processed = text_pre_process(user_text)
+        # Wrap into a DataFrame because pipeline expects 'Processed_text' column
+        input_df = pd.DataFrame({"Processed_text": [processed]})
+        
+        pred = model.predict(input_df)[0]
+        
+        if pred == 1:
+            st.warning("Alert!! This message is SPAM")
+        else:
+            st.success("This message is NOT_SPAM")
+                                                   
+
+'''https://github.com/shubhumre777'''
